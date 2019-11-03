89eca397282bba63c7f022f26c6f216db8f12bd5
sysu17364068 <56910978+sysu17364068@users.noreply.github.com>
2019-11-03 15:58:41 +0800

    Create dph.c

---
+++ dph.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex;
+pthread_cond_t cond_var;
+
+typedef struct Philosopher {
+	int num;
+	int condition;    	//为0时表示正在思考，为1时表示正在吃饭
+	int chop_z;	//为0时表示未被使用，为1时表示正在使用
+	int chop_y;
+	struct Philosopher* left_ph;
+	struct Philosopher* right_ph;
+	pthread_t act;
+} Phil;
+
+Phil* first = NULL;
+
+void pickup_forks(Phil* ph) {
+	pthread_mutex_lock(&mutex);
+	if(ph->chop_z==0 && ph->chop_y==0) {
+		ph -> chop_z = 1;
+		ph -> chop_y = 1;
+		ph -> condition = 1;
+		ph -> left_ph -> chop_y = 1;
+		ph -> right_ph -> chop_z = 1;
+		printf("%d号哲学家正在吃饭\n", ph->num);
+		sleep(1);
+	}
+	else {
+		pthread_cond_wait(&cond_var, &mutex);
+		pickup_forks(ph);
+	}
+	pthread_mutex_unlock(&mutex);
+}
+
+void return_forks(Phil* ph) {
+	int thinking = 0;
+	thinking = rand() % 5 + 1;
+	pthread_mutex_lock(&mutex);
+	ph -> chop_z = 0;
+	ph -> chop_y = 0;
+	ph -> condition = 0;
+	ph -> left_ph -> chop_y = 0;
+	ph -> right_ph -> chop_z = 0;
+
+	printf("%d号哲学家正在思考\n", ph->num);
+	pthread_cond_signal(&cond_var);
+	pthread_mutex_unlock(&mutex);
+	sleep(thinking);
+}
+
+void* behave(void *arg) {
+	Phil* ph = (Phil*) arg;
+	while(1) {
+		printf("%d号哲学家感到饥饿\n", ph->num);
+		pickup_forks(ph);
+		return_forks(ph);
+	}
+}
+
+int main(void) {
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&cond_var, NULL);
+
+	first = (Phil*) malloc(sizeof(Phil));
+	first -> num = 0;
+	first -> condition = 0;
+	first -> chop_z = 0;
+	first -> chop_y = 0;
+	first -> left_ph = NULL;
+	first -> right_ph = NULL;
+
+	Phil* ph = NULL;
+	Phil* tail = first;
+	for(int i=0; i<4; i++) {
+		ph = (Phil*) malloc(sizeof(Phil));
+		ph -> num = i+1;
+		ph -> condition = 0;
+		ph -> chop_z = 0;
+		ph -> chop_y = 0;
+		ph -> left_ph = NULL;
+		ph -> right_ph = NULL;
+
+		tail -> right_ph = ph;
+		tail = tail -> right_ph;
+		tail -> left_ph = ph;
+	}
+	tail -> right_ph = first;
+	first -> left_ph = tail;
+
+	ph = first;
+	for(int j=0; j<5; j++) {
+		pthread_create(&(ph->act), NULL, behave, (void*)ph);
+		ph = ph -> right_ph;
+	}
+
+	Phil* ph2 = first;
+	for(int i=0; i<5; i++) {
+		pthread_join(ph2->act, NULL);
+		ph2 = ph2 -> right_ph;
+	}
+	return 0;
+}
